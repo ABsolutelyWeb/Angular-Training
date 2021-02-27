f3d6d9a41df1ff1b34d3ff03740ed2b6d039c869
Abhishek Gupta <Abhishek.Gupta@forwardadvantage.com>
2021-02-26 19:15:15 -0800

    2/26

--- DatingApp/API/Data/DataContext.cs
+++ DatingApp/API/Data/DataContext.cs
@@ -1,10 +1,11 @@
+using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext ( [NotNullAttribute] DbContextOptions options) : base(options){
+        public DataContext (DbContextOptions options) : base(options){
 
         }
 
